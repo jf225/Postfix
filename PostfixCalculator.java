5695542d684faf73c0ba01d1a0245da3862d6a0b
James <ckfroste@gmail.com>
2020-11-02 13:18:59 -0700

    Added Constructor

---
+++ PostfixCalculator.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class PostfixCalculator<E> {
+    Scanner s = new Scanner(System.in);
+    private Stack main = new Stack();
+    private Stack temp = new Stack();
+
+    public PostfixCalculator(){}
+
+    public void fixOrder(){ }
+
+    public void addEquation(){ }
+
+    public void calculate(){}
+
+}
