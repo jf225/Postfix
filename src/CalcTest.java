b454f7eb01a85e147899b3aa1051e63ac2fa1c83
James <jfahey21@kentdenver.org>
2020-11-10 13:18:58 -0700

    Added print statements

--- src/CalcTest.java
+++ src/CalcTest.java
@@ -2,7 +2,7 @@ public class CalcTest {
     public static void main(String[] args) {
         PostfixCalculator p = new PostfixCalculator();
         // adding this so file is different for github
-        String s = new String("4 2 1 + *");
+        String s = new String("4 2 1 + -");
         p.setEquation(9, s);
         p.Calculate();
 }
